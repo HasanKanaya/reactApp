4bb8be0516016d957784e80c3b3f2853bb2435a0
Hasan Kanaya <hasan.kanaya9@gmail.com>
2023-12-25 22:33:53 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect, useRef } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Nothing from "./components/Nothing";
+
+function App() {
+  return (
+    <>
+      <h2>Hello World</h2>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Nothing.jsx
@@ -0,0 +1,9 @@
+function Nothing() {
+  return (
+    <>
+      <h2>Hello from Nothing component</h2>
+    </>
+  );
+}
+
+export default Nothing;
